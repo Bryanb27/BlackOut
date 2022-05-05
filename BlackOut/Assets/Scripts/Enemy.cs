7518d52c40efda9628b7d5932dc1c81153ed2bc5
Talkys <49161064+Talkys@users.noreply.github.com>
2022-05-05 14:43:35 -0300

    Classe inimigo genérico 1

---
+++ BlackOut/Assets/Scripts/Enemy.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour {
+
+	public float startSpeed = 10f;
+
+	[HideInInspector]
+	public float speed;
+
+	public float startHealth = 100;
+	private float health;
+  private floar speed;
+
+	private bool isDead = false;
+
+	void Start ()
+	{
+		speed = startSpeed;
+		health = startHealth;
+	}
+
+	public void TakeDamage (float amount)
+	{
+		health -= amount;
+
+		if (health <= 0 && !isDead)
+		{
+			Die();
+		}
+	}
+
+	public void Slow (float pct)
+	{
+		speed = startSpeed * (1f - pct);
+	}
+
+	void Die ()
+	{
+		isDead = true;
+
+		WaveSpawner.EnemiesAlive--;
+
+		Destroy(gameObject);
+	}
+
+}
